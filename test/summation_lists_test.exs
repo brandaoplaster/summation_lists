a278f9608f70099d7041720066347f2481f20629
brandaoplaster <brandaoplaster@gmail.com>
2021-03-21 22:01:16 -0300

    feat: adding test for sum list

--- test/summation_lists_test.exs
+++ test/summation_lists_test.exs
@@ -1,8 +1,19 @@
 defmodule SummationListsTest do
   use ExUnit.Case
-  doctest SummationLists
 
-  test "greets the world" do
-    assert SummationLists.hello() == :world
+  describe "call/1" do
+    test "returns the list sum" do
+      list = [1,2,3,4,5]
+
+      response = SummationLists.call(list)
+
+      expected_response = 15
+
+      assert response == expected_response
+    end
+
+    test "returns 0 when list empty list" do
+      assert SummationLists.call([]) == 0
+    end
   end
 end
